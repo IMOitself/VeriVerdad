d3b526942eec1df56a8f6dc310b1842b34e86dfb
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-02 21:55:43 +0800

    feat(migration): add complete migration requirements

--- backend/database/migrations/create_users_table.php
+++ backend/database/migrations/create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
 	{
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
-			$table->string('name');
+			$table->string('first_name');
+			$table->string('last_name');
 			$table->string('email')->unique();
 			$table->string('password');
+			$table->enum('role', ['student', 'teacher']);
+			$table->foreignId('section_id')->nullable()->constrained('sections')->nullOnDelete();
 			$table->timestamps();
 		});
 	}
