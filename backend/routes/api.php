e2eaa1ab62e160eca2cf27538a093160d17438d1
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-04 06:42:36 +0800

    feat(api): expand API routes for sections and badges management

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,15 +3,55 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BadgeController;
+use App\Http\Controllers\SectionController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VeribotController;
 
-Route::post('/register', [AuthController::class, 'register'])
-	->middleware('secret.token');
+Route::middleware('secret.token')->group(function () {
+	Route::post('/register', [AuthController::class, 'register']);
+	Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/login', [AuthController::class, 'login']);
+	Route::middleware('auth:sanctum')->group(function () {
+		Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->group(function () {
-	Route::post('/logout', [AuthController::class, 'logout']);
-	Route::get('/user', function (Request $request) {
-		return $request->user();
+		Route::get('/users', [UserController::class, 'index']);
+		Route::get('/user', [UserController::class, 'show']);
+		Route::get('/users/{id}', [UserController::class, 'show']);
+		Route::put('/users/{id?}', [UserController::class, 'update']);
+		Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+		Route::get('/veribot', [VeribotController::class, 'index']);
+		Route::post('/veribot', [VeribotController::class, 'analyze']);
+		Route::post('/veribot/submit', [VeribotController::class, 'submitQuiz']);
+		Route::get('/veribot/history', [VeribotController::class, 'history']);
+		Route::get('/veribot/{id}', [VeribotController::class, 'show']);
+		Route::delete('/veribot/{id}', [VeribotController::class, 'destroy']);
+
+		Route::get('/tasks', [TaskController::class, 'index']);
+		Route::post('/tasks', [TaskController::class, 'store']);
+		Route::get('/tasks/{id}', [TaskController::class, 'show']);
+		Route::put('/tasks/{id}', [TaskController::class, 'update']);
+		Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+		Route::post('/tasks/{id}/submit', [TaskController::class, 'submit']);
+		Route::get('/tasks/{id}/submissions', [TaskController::class, 'submissions']);
+		Route::delete('/tasks/{id}/submit', [TaskController::class, 'unsubmit']);
+
+		Route::get('/sections', [SectionController::class, 'index']);
+		Route::post('/sections', [SectionController::class, 'store']);
+		Route::get('/sections/{id}', [SectionController::class, 'show']);
+		Route::put('/sections/{id}', [SectionController::class, 'update']);
+		Route::delete('/sections/{id}', [SectionController::class, 'destroy']);
+		Route::get('/sections/{id}/stats', [SectionController::class, 'stats']);
+		Route::post('/sections/{id}/assign', [SectionController::class, 'assignStudent']);
+
+		Route::get('/badges', [BadgeController::class, 'index']);
+		Route::post('/badges', [BadgeController::class, 'store']);
+		Route::get('/badges/{id}', [BadgeController::class, 'show']);
+		Route::put('/badges/{id}', [BadgeController::class, 'update']);
+		Route::delete('/badges/{id}', [BadgeController::class, 'destroy']);
+		Route::post('/badges/{id}/attach', [BadgeController::class, 'attachUser']);
+		Route::delete('/badges/{id}/detach', [BadgeController::class, 'detachUser']);
 	});
 });
