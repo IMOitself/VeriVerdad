335bbc3a86aac035c6f033ce310720a107449c24
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-04 20:42:53 +0800

    feat(router): implement routing structure with landing, dashboard, and authentication pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './context/AuthContext'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './index.css';
+import Landing from './pages/Landing';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Veribot from './pages/Veribot';
+import Sources from './pages/Sources';
+import History from './pages/History';
+import Dashboard from './pages/Dashboard';
+import Statistics from './pages/Statistics';
 
-import './index.css'
-import App from './App.jsx'
+const router = createBrowserRouter([
+	{ path: '/', element: <Landing /> },
+	{ path: '/dashboard', element: <Dashboard /> },
+	{ path: '/login', element: <Login /> },
+	{ path: '/register', element: <Register /> },
+	{ path: '/home', element: <Home /> },
+	{ path: '/veribot', element: <Veribot /> },
+	{ path: '/sources', element: <Sources /> },
+	{ path: '/history', element: <History /> },
+	{ path: '/statistics', element: <Statistics /> },
+]);
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<AuthProvider>
-			<App />
-		</AuthProvider>
-	</StrictMode>,
-)
+		<RouterProvider router={router} />
+	</StrictMode>
+);
