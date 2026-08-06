5c6d070a29d22a463e169d08f27f710ab70548e3
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-06 19:16:47 +0800

    feat: add main application routing in App component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Landing from './pages/Landing'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Veribot from './pages/Veribot'
+import Sources from './pages/Sources'
+import History from './pages/History'
+import Dashboard from './pages/Dashboard'
+import Statistics from './pages/Statistics'
+import ProtectedRoute from './utils/ProtectedRoute'
+
+const router = createBrowserRouter([
+	{ path: '/', element: <Landing /> },
+	{ path: '/login', element: <Login /> },
+	{ path: '/register', element: <Register /> },
+	{
+		element: <ProtectedRoute />,
+		children: [
+			{ path: '/dashboard', element: <Dashboard /> },
+			{ path: '/home', element: <Home /> },
+			{ path: '/veribot', element: <Veribot /> },
+			{ path: '/sources', element: <Sources /> },
+			{ path: '/history', element: <History /> },
+			{ path: '/statistics', element: <Statistics /> },
+		]
+	}
+])
+
+export default function App() {
+	return <RouterProvider router={router} />
+}
