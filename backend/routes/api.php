609793adec779654ee05101b565ff1d6051344f4
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-07 21:57:47 +0800

    refactor(api): add new routes and remove redundant routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,35 +10,21 @@ use App\Http\Controllers\VeribotController;
 
 Route::middleware('secret.token')->group(function () {
 	Route::post('/register', [AuthController::class, 'register']);
-	Route::post('/login', [AuthController::class, 'login']);
+	Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 	Route::middleware('auth:sanctum')->group(function () {
 		Route::post('/logout', [AuthController::class, 'logout']);
 		Route::patch('/profile', [UserController::class, 'updateProfile']);
 		Route::get('/profile', [UserController::class, 'showProfile']);
 
+		Route::get('/users', [UserController::class, 'index']);
+		Route::patch('/users/{id}', [UserController::class, 'update']);
+		Route::delete('/users/{id}', [UserController::class, 'destroy']);
 		Route::get('/veribot', [VeribotController::class, 'index']);
 		Route::post('/veribot', [VeribotController::class, 'analyze']);
 		Route::post('/veribot/submit', [VeribotController::class, 'submitQuiz']);
 		Route::get('/veribot/history', [VeribotController::class, 'history']);
 		Route::get('/veribot/{id}', [VeribotController::class, 'show']);
-		Route::delete('/veribot/{id}', [VeribotController::class, 'destroy']);
-
-		Route::get('/tasks', [TaskController::class, 'index']);
-		Route::post('/tasks', [TaskController::class, 'store']);
-		Route::get('/tasks/{id}', [TaskController::class, 'show']);
-		Route::put('/tasks/{id}', [TaskController::class, 'update']);
-		Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-		Route::post('/tasks/{id}/submit', [TaskController::class, 'submit']);
-		Route::get('/tasks/{id}/submissions', [TaskController::class, 'submissions']);
-		Route::delete('/tasks/{id}/submit', [TaskController::class, 'unsubmit']);
-
-		Route::get('/badges', [BadgeController::class, 'index']);
-		Route::post('/badges', [BadgeController::class, 'store']);
-		Route::get('/badges/{id}', [BadgeController::class, 'show']);
-		Route::put('/badges/{id}', [BadgeController::class, 'update']);
-		Route::delete('/badges/{id}', [BadgeController::class, 'destroy']);
-		Route::post('/badges/{id}/attach', [BadgeController::class, 'attachUser']);
-		Route::delete('/badges/{id}/detach', [BadgeController::class, 'detachUser']);
-	});
+		Route::delete('/veribot/{id}', [VeribotController::class, 'destroy']); 
+		});
 });
