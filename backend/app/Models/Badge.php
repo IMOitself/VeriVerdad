46cdb5a54fa1f7fdf5860472ccf1e75edb6870df
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-08 23:43:13 +0800

    feat(badge): remove icon attribute from fillable properties

--- backend/app/Models/Badge.php
+++ backend/app/Models/Badge.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['name', 'description', 'icon'])]
+#[Fillable(['name', 'description'])]
 class Badge extends Model
 {
 	use HasFactory;
