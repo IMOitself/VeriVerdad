b304e2ace05a61f3d33351ce4b8bc92fe5e44fb6
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-08 23:43:29 +0800

    feat(seeder): add SectionSeeder

---
+++ backend/database/seeders/SectionSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Section;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SectionSeeder extends Seeder
+{
+	public function run(): void
+	{
+		$teacher = User::firstOrCreate(
+			['email' => 'teacher@laverdad.edu.ph'],
+			[
+				'username' => 'prof_mil_advisor',
+				'password' => Hash::make('teacher123'),
+				'role'     => 'teacher',
+			]
+		);
+
+		$sectionsData = [
+			['name' => '12 - Galatians', 'code' => '12-GAL', 'teacher_id' => $teacher->id],
+			['name' => '12 - Corinthians', 'code' => '12-COR', 'teacher_id' => $teacher->id],
+			['name' => '12 - Ephesians', 'code' => '12-EPH', 'teacher_id' => $teacher->id],
+		];
+
+		$createdSections = [];
+		foreach ($sectionsData as $s) {
+			$createdSections[] = Section::updateOrCreate(['code' => $s['code']], $s);
+		}
+
+		// Assign students into sections
+		$students = User::where('role', 'student')->get();
+		foreach ($students as $idx => $student) {
+			$section = $createdSections[$idx % count($createdSections)];
+			$student->update(['section_id' => $section->id]);
+		}
+	}
+}
