042e19ddc4dfb0ebc6bb8e6cf44579664bf04543
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-09 00:01:15 +0800

    refactor(ProtectedRoute): formatting

--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
-import { Outlet, Navigate } from 'react-router';
+import { Outlet, Navigate } from 'react-router'
 
 export default function ProtectedRoute() {
-	const token = localStorage.getItem('token');
-	return token ? <Outlet /> : <Navigate to="/login" />;
+	const token = localStorage.getItem('token')
+	return token ? <Outlet /> : <Navigate to="/" />
 }
