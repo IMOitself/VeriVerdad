7fe90f0aab8c03c27b4ed05bfbfe607681643bc0
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-11 07:08:17 +0800

    feat: add api trat

--- backend/app/Http/Traits/ApiResponder.php
+++ backend/app/Http/Traits/ApiResponder.php
@@ -4,7 +4,13 @@ namespace App\Http\Traits;
 
 trait ApiResponder
 {
-	protected function success() {}
+	protected function success($data = null, $message = null, $status = 200)
+	{
+		return response()->json(['status' => 'success', 'message' => $message, 'data' => $data], $status);
+	}
 
-	protected function error() {}
+	protected function error($message = null, $status = 400, $errors = null)
+	{
+		return response()->json(['status' => 'error', 'message' => $message, 'errors' => $errors], $status);
+	}
 }
