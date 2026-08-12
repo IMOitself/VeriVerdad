1e41c7bd806e458647b57a5b9a66262beed5f8ee
zamuwelle <226855307+zamuwelle@users.noreply.github.com>
2026-08-13 06:27:06 +0800

    feat: add TrashIcon component to Icons

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -51,3 +51,9 @@ export const CopyIcon = props => (
 		<path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" />
 	</svg>
 )
+
+export const TrashIcon = props => (
+	<svg height="24" viewBox="0 -960 960 960" width="24" fill="currentColor" {...props}>
+		<path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+	</svg>
+)
